2a731d8678548358b884fdef108170710a390f3e
Karim-Errahem <Karim.arrahem@isaeg.u-gafsa.tn>
2024-10-26 00:12:25 +0100

    First Commit

---
+++ components/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Liens rapides</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> accueil </a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> à propos</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> boutique</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>liens supplémentaires</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> connexion</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> enregistrer</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> panier</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i>commandes</a>
+      </div>
+
+      <div class="box">
+         <h3>Contactez-nous</h3>
+         <a href="tel:1234567890"><i class="fab fa-phone"></i> 76 221 700</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> 76 221 700</a>
+         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> mediaplus.gafsa@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> 
+Av.Jamel Abdennaceur Gafsa, Gafsa, Tunisia </a>
+      </div>
+
+      <div class="box">
+         <h3>Suivez-nous</h3>
+         <a href="https://www.facebook.com/MediaPlusGafsa"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. errahem karim</span> | all rights reserved!</div>
+
+</footer>
